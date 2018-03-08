690d3227d34b1fef865129900e06823fe21439a2
andrea silvi <andrea silvi>
2018-03-08 22:51:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/andrea/custom/widgets/MenuButton.java
@@ -0,0 +1,25 @@
+package com.example.andrea.custom.widgets;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+/**
+ * Created by Andrea on 08/03/2018.
+ */
+
+
+public class MenuButton extends AppCompatButton {
+
+    public MenuButton(Context context) {
+        super(context);
+    }
+
+    public MenuButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MenuButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
